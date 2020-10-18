ba7766c28c209775c5b116b60cedea0459c2b405
DanielaFlore <47197880+DanielaFlore@users.noreply.github.com>
2020-10-18 01:26:42 -0600

    file transfer w/ proxy

---
+++ file-transfer-lab/transfer-w-fork/ServerProxy.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python3
+
+import sys,os
+sys.path.append("../lib")       # for params
+import re, socket, params
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+bindAddr = ("127.0.0.1", listenPort)
+lsock.bind(bindAddr)
+lsock.listen(5)
+print("listening on:", bindAddr)
+
+sock, addr = lsock.accept()
+
+print("connection rec'd from", addr)
+from framedSock import framedSend, framedReceive
+#################################################
+fileName = framedReceive(sock, debug).decode()
+print("filename is ", fileName)
+conf = "filename received and stored"
+framedSend(sock, conf.encode(), debug)
+#buggy:(
+path = os.getcwd()
+filesPath=path+'\\'+fileName
+print(filesPath)
+if(os.path.isfile(filesPath)):
+    print("file already exists in server....exiting")
+    framedSend(sock, b"file exists", debug)
+    sys.exit(0)
+framedSend(sock, b"pass", debug)
+f = open(fileName, "w")
+################################################
+
+print("starting to write file....")
+while True:
+    payload = framedReceive(sock, debug)
+    if debug: print("rec'd: ", payload)
+    if payload == b"end of file":
+        closing = "end of file reached!"
+        framedSend(sock, closing.encode(), debug)
+        f.close()
+        break
+    f.write(payload.decode())
+
+    if not payload:
+        f.close()
+        break
+    payload += b"!"             # make emphatic!
+    framedSend(sock, payload, debug)
+print("end")
